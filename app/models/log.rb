7127fba00d58dfde8503f86f2b3a675566215377
Chiang Fong Lee <myself@cflee.net>
2015-03-08 10:53:01 +0800

    Fix indentation

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,5 @@
 class Log < ActiveRecord::Base
-        validates :date, presence: true
-        validates :duration, presence: true, numericality: { greater_than: 0 }
+  validates :date, presence: true
   validates :description, presence: true
+  validates :duration, presence: true, numericality: { greater_than: 0 }
 end
