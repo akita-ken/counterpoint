e9ebc6729ab201eb8aa310e4530b4a538bd953ba
Chiang Fong Lee <myself@cflee.net>
2015-03-08 10:52:21 +0800

    Convert tabs to 2 spaces

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,49 +1,49 @@
 class LogsController < ApplicationController
-	def index
-		@logs = Log.all
-	end
+  def index
+    @logs = Log.all
+  end
 
-	def new
-		@log = Log.new
-	end
+  def new
+    @log = Log.new
+  end
 
-	def edit
-		@log = Log.find(params[:id])
-	end
+  def edit
+    @log = Log.find(params[:id])
+  end
 
-	def destroy
-		@log = Log.find(params[:id])
-		@log.destroy
+  def destroy
+    @log = Log.find(params[:id])
+    @log.destroy
 
-		redirect_to log_path
-	end
+    redirect_to log_path
+  end
 
-	def create
-		@log = Log.new(log_params)
+  def create
+    @log = Log.new(log_params)
 
-		if @log.save
-			redirect_to @log
-		else
-			render 'new'
-		end
-	end
+    if @log.save
+      redirect_to @log
+    else
+      render 'new'
+    end
+  end
 
-	def update
-		@log = Log.find(params[:id])
+  def update
+    @log = Log.find(params[:id])
 
-		if @log.update(log_params)
-			redirect_to @log
-		else
-			render 'edit'
-		end		
-	end
+    if @log.update(log_params)
+      redirect_to @log
+    else
+      render 'edit'
+    end
+  end
 
-	def show
-		@log = Log.find(params[:id])
-	end
+  def show
+    @log = Log.find(params[:id])
+  end
 
-	private
-		def log_params
-			params.require(:log).permit(:date, :description, :duration)
-		end
+  private
+    def log_params
+      params.require(:log).permit(:date, :description, :duration)
+    end
 end
