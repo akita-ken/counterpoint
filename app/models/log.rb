369148486baf303322bab83ce66b7963601cd51d
Chiang Fong Lee <myself@cflee.net>
2015-03-08 10:54:14 +0800

    Change log model validation
    Allow 0 as numerical value for duration

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,5 @@
 class Log < ActiveRecord::Base
   validates :date, presence: true
   validates :description, presence: true
-  validates :duration, presence: true, numericality: { greater_than: 0 }
+  validates :duration, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
