7f7eb79e4d61ecb825a7f7a71ab9124eb04cc7ff
Chiang Fong Lee <myself@cflee.net>
2015-03-10 01:54:26 +0800

    Rework email parsing to handle duration properly
    Remove the duration from the description if it's detected as such.

--- app/controllers/reply_inbox_controller.rb
+++ app/controllers/reply_inbox_controller.rb
@@ -11,10 +11,20 @@ class ReplyInboxController < ApplicationController
     user = User.find_by_email(from_email)
     unless user.nil?
       parsed_body.lines.each do |line|
+        duration = 0.0
+        description = line.chomp
+        first_word = description.split.first
+        if (true if Float(first_word) rescue false)
+          # grab the duration as a number
+          duration = first_word.to_f
+          # remove the first word
+          description = description.split[1..-1].join(' ')
+        end
+
         log = user.logs.new
-        log.description = line.chomp
-        log.date = Date.current
-        log.duration = line.split.first.to_f
+        log.description = description
+        log.date = Date.current # current assumption
+        log.duration = duration
         log.save
       end
     end
