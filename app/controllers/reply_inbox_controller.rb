1b1ca9e6ea59058360b0d534bea3e375726d19df
Chiang Fong Lee <myself@cflee.net>
2015-03-09 22:12:54 +0800

    Read msg text instead of msg

--- app/controllers/reply_inbox_controller.rb
+++ app/controllers/reply_inbox_controller.rb
@@ -3,9 +3,9 @@ class ReplyInboxController < ApplicationController
   authenticate_with_mandrill_keys! ENV["MANDRILL_WEBHOOK_KEY"]
 
   def handle_inbound(event_payload)
-    logger.debug "Message:: #{event_payload['msg']}"
+    logger.debug "Message:: #{event_payload['msg']['text']}"
 
-    mail = Mail.read_from_string(event_payload['msg'])
+    mail = Mail.read_from_string(event_payload['msg']['raw_msg'])
     logger.debug "Mail:: #{mail.text_part}"
   end
 end
