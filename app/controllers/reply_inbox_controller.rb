da8786c492e569bc8c2e80b14e35bbcb53245e21
Chiang Fong Lee <myself@cflee.net>
2015-03-09 22:31:08 +0800

    Rudimentary parsing and ingest of email replies
    Quick hack: Find user by email address, assume current date.
    
    This should use a unique reply-to email address per user (like emailing to Evernote) for anti-spoofing, or a unique reply-to per email prompt that is sent, in order to match to the correct day.

--- app/controllers/reply_inbox_controller.rb
+++ app/controllers/reply_inbox_controller.rb
@@ -3,7 +3,20 @@ class ReplyInboxController < ApplicationController
   authenticate_with_mandrill_keys! ENV["MANDRILL_WEBHOOK_KEY"]
 
   def handle_inbound(event_payload)
-    logger.debug "Message:: #{event_payload['msg']['text']}"
+    logger.debug "From:: #{event_payload['msg']['from_email']}"
+    logger.debug "Message text:: #{event_payload['msg']['text']}"
+    from_email = event_payload['msg']['from_email']
+    parsed_body = EmailReplyParser.parse_reply(event_payload['msg']['text'])
 
+    user = User.find_by_email(from_email)
+    unless user.nil?
+      parsed_body.each do |line|
+        log = user.logs.new
+        log.description = line
+        log.date = Date.current
+        log.duration = 0.0
+        log.save
+      end
+    end
   end
 end
