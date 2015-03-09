5fa2e216e7496ea36f9a21fac362ab2277c1eb1f
Chiang Fong Lee <myself@cflee.net>
2015-03-09 21:58:34 +0800

    Try to parse text/plain bit out from email

--- app/controllers/reply_inbox_controller.rb
+++ app/controllers/reply_inbox_controller.rb
@@ -1,4 +1,11 @@
 class ReplyInboxController < ApplicationController
   include Mandrill::Rails::WebHookProcessor
   authenticate_with_mandrill_keys! ENV["MANDRILL_WEBHOOK_KEY"]
+
+  def handle_inbound(event_payload)
+    logger.debug "Message:: #{event_payload['msg']}"
+
+    mail = Mail.read_from_string(event_payload['msg'])
+    logger.debug "Mail:: #{mail.text_part}"
+  end
 end
