4c7405353d2fed9d738e71d9febe07e62b66835d
Chiang Fong Lee <myself@cflee.net>
2015-03-09 23:24:26 +0800

    Add daily prompt email as a rake task
    - Add the email as a HTML view.
    - Rake task calls method on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'mandrill' # mandrill-api gem
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -5,4 +7,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :logs, dependent: :destroy
+
+  def send_daily_emails
+    m = Mandrill::API.new
+    message = {
+      :from_name => "Counterpoint",
+      :from_email => "reply@counterpoint.cflee.net",
+      :to => User.all.map { |u| {:email => u.email, :name => u.email, :type => 'to'} }
+      :subject => "What did you do today? - #{Date.current.to_formatted_s(:rfc822)}",
+      :html => render_to_string('mailer/daily_prompt', :layout => false),
+      :merge_vars => [],
+      :preserve_recipients => false
+    }
+    sending = m.messages.send message
+  end
 end
---
+++ lib/tasks/scheduler.rake
@@ -0,0 +1,6 @@
+desc "This task is called by the Heroku scheduler add-on"
+task :send_daily_emails => :environment do
+  puts "Sending daily emails..."
+  User.send_reminders
+  puts "Done."
+end
